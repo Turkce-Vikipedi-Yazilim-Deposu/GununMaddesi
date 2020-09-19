4262927346dfe6bfa152761f506828618cede9e7
M. Serdar Karaman <serdar@karaman.dev>
2020-09-19 20:05:43 +0300

    Comment'ler yalancı

--- deneme.py
+++ deneme.py
@@ -4,7 +4,6 @@
 # gerekli kütüphaneleri dahil et
 import pywikibot
 import locale
-import json
 
 # locale ayarı
 locale.setlocale(locale.LC_ALL, 'tr_TR.utf8')
@@ -12,17 +11,11 @@ locale.setlocale(locale.LC_ALL, 'tr_TR.utf8')
 site = pywikibot.Site('tr', 'wikipedia')
 page = pywikibot.Page(site, "Vikipedi:Deneme tahtası")
 
-# Giriş denemesini sonucunu ekrana JSON ile düzenleyerek yazdır
-print(page.text)
-
-# 2 bölümü birbirinden ayır
-print("\n-------------------------------------------------------------------------\n")
-
 # Deneme sayfasına mesaj ekle.
 page.text += '\n== ' + site.username() + ' test ==\nDeneme deneme 123 --~~~~'
 page.save(site.username() + " ile deneme mesajı ekleniyor")
 
-# Sonucu ekrana JSON ile düzenleyerek yazdır
+# Sayfanın son halini ekrana yazdır
 print(page.text)
 
 # Programı kapat
